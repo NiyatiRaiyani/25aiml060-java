dacf5068fa2d759d9024b90ac24eb329fb422f5c
NiyatiRaiyani <niyatiraiyani42@gmail.com>
2026-08-07 13:38:48 +0530

    Implement Password Strength Checker with validation methods and update Driver class to utilize them

--- Lab-04/Password Strength Checker/Driver.java
+++ Lab-04/Password Strength Checker/Driver.java
@@ -1,1 +1,39 @@
+public class Driver 
+{
+     public static void main(String[] args) 
+     {
 
+        String[] passwords = 
+        {
+                "abc",
+                "Hello12",
+                "Abcd1234!",
+                "password",
+                "Niyati1252",
+                "Java@45"
+        };
+        
+        for (String pw : passwords) 
+            {
+
+            System.out.println("\nPassword : " + pw);
+
+            System.out.println("Length : " +
+                    PasswordChecker.hasLength(pw));
+
+            System.out.println("Uppercase : " +
+                    PasswordChecker.hasUpperCase(pw));
+
+            System.out.println("Digit : " +
+                    PasswordChecker.hasDigit(pw));
+
+            System.out.println("Special : " +
+                    PasswordChecker.hasSpecial(pw));
+
+            System.out.println("Strength : " +
+                    PasswordChecker.strength(pw));
+        }
+
+    }
+
+}
---
+++ Lab-04/Password Strength Checker/PasswordChecker.java
@@ -0,0 +1,43 @@
+public class PasswordChecker
+{
+    public static boolean hasLength(String pw)
+    {
+        return pw.length() >= 8;
+    }
+     public static boolean hasUpperCase(String pw) 
+    {
+        return pw.matches(".*[A-Z].*");
+    }
+
+    public static boolean hasDigit(String pw) 
+    {
+        return pw.matches(".*\\d.*");
+    }
+
+    public static boolean hasSpecial(String pw) 
+    {
+        return pw.matches(".*[^a-zA-Z0-9].*");
+    }
+
+    public static String strength(String pw) 
+    {
+    int count = 0;
+
+        if (hasLength(pw))
+            count++;
+        if (hasUpperCase(pw))
+            count++;
+        if (hasDigit(pw))
+            count++;
+        if (hasSpecial(pw))
+            count++;
+
+        if (count <= 1)
+            return "Weak";
+        else if (count <= 3)
+            return "Medium";
+        else
+            return "Strong";
+    }
+
+}
