a2e0008c68d9d22051ed523f40bf7075b8b1c74a
NiyatiRaiyani <niyatiraiyani42@gmail.com>
2026-08-21 10:32:50 +0530

    Add DivideByZeroException and InvalidNumberException classes to GuardedCalculator

---
+++ Lab-08/GuardedCalculator.java
@@ -0,0 +1,18 @@
+
+class DivideByZeroException extends Exception {
+    DivideByZeroException(String msg) {
+        super(msg);
+    }
+}
+
+class InvalidNumberException extends Exception {
+    InvalidNumberException(String msg) {
+        super(msg);
+    }
+}
+
+public class GuardedCalculator{
+    public static void main(String[] args) {
+        
+    }
+}
