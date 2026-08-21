a5c1504fccad8cd8df69c6e058ba9afce2b960cc
NiyatiRaiyani <niyatiraiyani42@gmail.com>
2026-08-21 10:57:02 +0530

    Refactor GuardedCalculator to improve operator handling and exception messages

--- Lab-08/GuardedCalculator.java
+++ Lab-08/GuardedCalculator.java
@@ -10,56 +10,81 @@ class DivideByZeroException extends Exception {
 class InvalidNumberException extends Exception {
     InvalidNumberException(String msg) {
         super(msg);
-    }
+    } 
 }
 
-public class GuardedCalculator{
+public class GuardedCalculator {
     public static void main(String[] args) {
+
         try (Scanner sc = new Scanner(System.in)) {
+
             boolean done = false;
-            
+
             while (!done) {
                 try {
                     System.out.print("Enter Operand and Operator: ");
-                    
+
                     int a = sc.nextInt();
                     char op = sc.next().charAt(0);
                     int b = sc.nextInt();
-                    
-                    if (a < 0 || b < 0)
-                        throw new InvalidNumberException("Negative number is not allowed!");
-                    
-                    if (op == '*' && (a == 0 || b == 0))
-                        throw new InvalidNumberException("0 is not allowed in multiplication!");
-                    
-                    if (op == '/' && b == 0)
-                        throw new DivideByZeroException("DividebyZeroException ! Cannot divide by zero!");
-                    
-                    switch (op) {
-                        case '+' -> System.out.println("Result = " + (a + b));
-                        case '-' -> System.out.println("Result = " + (a - b));
-                        case '*' -> System.out.println("Result = " + (a * b));
-                        case '/' -> System.out.println("Result = " + (a / b));
-                        default -> System.out.println("Invalid operator!");
+
+                    if (a < 0 || b < 0) {
+                        throw new InvalidNumberException(
+                            "Negative number is not allowed!"
+                        );
                     }
-                    
-                    done = true;
+
+                    if (op == '*' && (a == 0 || b == 0)) {
+                        throw new InvalidNumberException(
+                            "0 is not allowed in multiplication!"
+                        );
+                    }
+
+                    if (op == '/' && b == 0) {
+                        throw new DivideByZeroException(
+                            "Cannot divide by zero!"
+                        );
+                    }
+
+                    if (op == '+') {
+                        System.out.println("Result = " + (a + b));
+                        done = true;
+                    }
+                    else if (op == '-') {
+                        System.out.println("Result = " + (a - b));
+                        done = true;
+                    }
+                    else if (op == '*') {
+                        System.out.println("Result = " + (a * b));
+                        done = true;
+                    }
+                    else if (op == '/') {
+                        System.out.println("Result = " + (a / b));
+                        done = true;
+                    }
+                    else {
+                        System.out.println("Invalid operator!");
+                    }
                 }
-                        
-                catch (InvalidNumberException | DivideByZeroException e) {
+
+                catch (InvalidNumberException |
+                       DivideByZeroException e) {
+
                     System.out.println(e.getMessage());
                     sc.nextLine();
                 }
-                
+
                 catch (InputMismatchException e) {
-                    System.out.println("Please enter valid numbers!");
+                    System.out.println(
+                        "Please enter valid numbers!"
+                    );
                     sc.nextLine();
                 }
-                
+
                 finally {
                     System.out.println("Attempt logged.\n");
-                } 
-            }           
+                }
+            }
         }
     }
 }
