be3fa9924996e0e468def68ee3722cfc2b9c1df0
NiyatiRaiyani <niyatiraiyani42@gmail.com>
2026-09-18 14:07:58 +0530

    Refactor NotificationDemo to improve message handling; add EmailSender and SmsSender classes for notification delivery

--- Lab-06/NotificationDemo.java
+++ Lab-06/NotificationDemo.java
@@ -1,40 +1,34 @@
 @FunctionalInterface
-interface Notifier {
+interface Notifier 
+{
     void send(String message);
 }
 
-interface Urgent {
+interface Urgent 
+{
 }
 
-class UrgentNotifier implements Urgent {
-}
+public class NotificationDemo 
+{
+    public static void main(String[] args) 
+    {   
 
-public class NotificationDemo {
-    public static void main(String[] args) {
+        Notifier email = message -> System.out.println("Email: " + message);
 
-        // Email sender using lambda
-        Notifier email = message ->
-                System.out.println("Email: " + message);
+        Notifier sms = message -> System.out.println("SMS: " + message);
 
-        // SMS sender using lambda
-        Notifier sms = message ->
-                System.out.println("SMS: " + message);
+        Notifier[] senders = { email, sms };
 
-        // Store senders in array
-        Notifier[] senders = {email, sms};
+        String message = "Your account has been updated.";
 
-        String message = "Your exam is tomorrow.";
-
-        // Broadcast message
-        for (Notifier sender : senders) {
+        for (Notifier sender : senders) 
+        {
             sender.send(message);
+
+            if (sender instanceof Urgent) 
+            {
+                sender.send(message);
+            }
         }
-
-        // Urgent notification
-        UrgentNotifier urgent = new UrgentNotifier();
-
-        System.out.println("Urgent Notification:");
-        email.send(message);
-        email.send(message);
     }
 }
