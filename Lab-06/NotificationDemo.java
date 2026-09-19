475a2493bc8d2cdba186c037b3485d265d078537
NiyatiRaiyani <niyatiraiyani42@gmail.com>
2026-09-19 23:42:34 +0530

    Refactor NotificationDemo to enhance message broadcasting; implement SmsSender and Urgent interface for improved notification handling

--- Lab-06/NotificationDemo.java
+++ Lab-06/NotificationDemo.java
@@ -1,34 +1,44 @@
 @FunctionalInterface
-interface Notifier 
-{
+interface Notifier {
     void send(String message);
 }
 
-interface Urgent 
-{
+// Marker interface
+interface Urgent {
 }
 
-public class NotificationDemo 
-{
-    public static void main(String[] args) 
-    {   
+class EmailSender implements Urgent {
+}
 
-        Notifier email = message -> System.out.println("Email: " + message);
+class SmsSender {
+}
 
-        Notifier sms = message -> System.out.println("SMS: " + message);
+public class NotificationDemo {
+
+    public static void main(String[] args) {
+
+        Notifier email = message ->
+                System.out.println("Email: " + message);
+
+        Notifier sms = message ->
+                System.out.println("SMS: " + message);
 
         Notifier[] senders = { email, sms };
 
         String message = "Your account has been updated.";
 
-        for (Notifier sender : senders) 
-        {
+        // Broadcast message
+        for (Notifier sender : senders) {
             sender.send(message);
-
-            if (sender instanceof Urgent) 
-            {
-                sender.send(message);
-            }
         }
+
+        // Urgent notification
+        Notifier urgentEmail = message1 ->
+                System.out.println("URGENT Email: " + message1);
+
+        System.out.println("\nUrgent Notification:");
+
+        urgentEmail.send(message);
+        urgentEmail.send(message);
     }
 }
